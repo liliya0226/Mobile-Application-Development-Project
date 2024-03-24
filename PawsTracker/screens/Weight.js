9eed3d7fb7862ed30727866bda6c771d4aa07cdd
xinyue <selenazheng3@gmail.com>
2024-03-24 12:30:36 -0700

    add weight navigation

--- PawsTracker/screens/Weight.js
+++ PawsTracker/screens/Weight.js
@@ -1,11 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import WeightItem from "../components/WeightItem";
+import {
+  getWeightFromDB,
+  writeWeightToDB,
+  deleteWeightFromDB,
+} from "../firebase-files/firestoreHelper";
 
-export default function Weight() {
+export default function Weight({ navigation }) {
+  const [weight, setWeights] = useState([]);
+
+  function receiveInput(data, date) {
+    const newGoal = { text: data, date: date };
+    writeWeightToDB(newGoal, "weights");
+  }
+
+  function goalPressHandler(weightItem) {
+    navigation.navigate("WeightDetails", { data: weightItem });
+  }
+  function goalDeleteHandler(deletedId) {
+    console.log("deleted", deletedId);
+    deleteWeightFromDB(deletedId);
+  }
   return (
     <View>
-      <Text>Weight</Text>
-      <Text>Add Weight</Text>
+      <View style={styles.bottomView}>
+        <FlatList
+          contentContainerStyle={styles.scrollViewContent}
+          data={weight}
+          renderItem={({ item }) => {
+            return (
+              <WeightItem
+                weightObj={item}
+                deleteFunction={goalDeleteHandler}
+                detailFunction={goalPressHandler}
+              />
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
